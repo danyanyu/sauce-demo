fc833b186f9e09b60c5d16ef6cd45388891dd8d2
Даниил Кирьянов <dakiryanov@miem.hse.ru>
2025-02-20 18:03:17 +0300

    open and close chrome

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,12 +2,17 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
+import static java.lang.Thread.sleep;
+
 public class LoginTest {
 
     @Test
-    public void checkLogin(){
+    public void checkLogin() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://saucedemo.com/");
+
+        sleep(1000);
+
         driver.quit();
     }
 }
